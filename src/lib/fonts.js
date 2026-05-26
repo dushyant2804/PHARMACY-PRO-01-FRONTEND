fe07f86043ac57f9239691ed19f6e8384d9ddb11
dushyant2804 <dushyantbhadu07@gmail.com>
2026-05-26 18:07:57 +0530

    Create fonts.js

---
+++ src/lib/fonts.js
@@ -0,0 +1,18 @@
+export const fonts = {
+  inter: {
+    name: "Inter (Clean)",
+    value: '"IBM Plex Sans", sans-serif',
+  },
+  playfair: {
+    name: "Playfair (Classic)",
+    value: '"Playfair Display", serif',
+  },
+  cormorant: {
+    name: "Cormorant (Elegant)",
+    value: '"Cormorant Garamond", serif',
+  },
+  calligraphy: {
+    name: "Calligraphy (Accent)",
+    value: '"Great Vibes", cursive',
+  },
+};
