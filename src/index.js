bbd78e3c81df089169be3852c52409bbb1f23dba
dushyant2804 <dushyantbhadu07@gmail.com>
2026-05-26 18:18:14 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
+import { AuthProvider } from "@/contexts/AuthContext";
+import { FontProvider } from "@/contexts/FontContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <AuthProvider>
+      <FontProvider>
+        <App />
+      </FontProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
