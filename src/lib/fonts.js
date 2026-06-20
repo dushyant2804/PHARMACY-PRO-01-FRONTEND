39d1d23ba9bc8133e9c680e3cb604e5c634d7ea8
dushyant2804 <dushyantbhadu07@gmail.com>
2026-06-20 18:28:01 +0530

    Fix settings font UI regressions

--- src/lib/fonts.js
+++ src/lib/fonts.js
@@ -1,19 +1,11 @@
 export const fonts = {
   ibmPlex: {
-    name: "IBM Plex Sans (Current)",
+    name: "IBM Plex Sans",
     value: '"IBM Plex Sans", sans-serif',
   },
   inter: {
     name: "Inter",
     value: '"Inter", sans-serif',
-  },
-  poppins: {
-    name: "Poppins",
-    value: '"Poppins", sans-serif',
-  },
-  montserrat: {
-    name: "Montserrat",
-    value: '"Montserrat", sans-serif',
   },
   roboto: {
     name: "Roboto",
@@ -23,52 +15,75 @@ export const fonts = {
     name: "Open Sans",
     value: '"Open Sans", sans-serif',
   },
-  lato: {
-    name: "Lato",
-    value: '"Lato", sans-serif',
-  },
-  nunito: {
-    name: "Nunito",
-    value: '"Nunito", sans-serif',
+  montserrat: {
+    name: "Montserrat",
+    value: '"Montserrat", sans-serif',
   },
   sourceSans3: {
     name: "Source Sans 3",
     value: '"Source Sans 3", sans-serif',
   },
-  merriweather: {
-    name: "Merriweather",
-    value: '"Merriweather", serif',
-  },
-  playfair: {
-    name: "Playfair Display",
-    value: '"Playfair Display", serif',
-  },
-  ubuntu: {
-    name: "Ubuntu",
-    value: '"Ubuntu", sans-serif',
-  },
   workSans: {
     name: "Work Sans",
     value: '"Work Sans", sans-serif',
   },
-  raleway: {
-    name: "Raleway",
-    value: '"Raleway", sans-serif',
+  lato: {
+    name: "Lato",
+    value: '"Lato", sans-serif',
+  },
+  merriweather: {
+    name: "Merriweather",
+    value: '"Merriweather", serif',
   },
   cabin: {
     name: "Cabin",
     value: '"Cabin", sans-serif',
   },
+  ubuntu: {
+    name: "Ubuntu",
+    value: '"Ubuntu", sans-serif',
+  },
   ptSans: {
     name: "PT Sans",
     value: '"PT Sans", sans-serif',
   },
+  poppins: {
+    name: "Poppins",
+    value: '"Poppins", sans-serif',
+  },
+  raleway: {
+    name: "Raleway",
+    value: '"Raleway", sans-serif',
+  },
+  playfair: {
+    name: "Playfair Display",
+    value: '"Playfair Display", serif',
+  },
   cormorant: {
-    name: "Cormorant Garamond (Legacy)",
+    name: "Cormorant Garamond",
     value: '"Cormorant Garamond", serif',
+  },
+  nunito: {
+    name: "Nunito",
+    value: '"Nunito", sans-serif',
+  },
+  josefinSans: {
+    name: "Josefin Sans",
+    value: '"Josefin Sans", sans-serif',
   },
   calligraphy: {
     name: "Great Vibes (Legacy Accent)",
     value: '"Great Vibes", cursive',
   },
 };
+
+export const fontGroups = {
+  Professional: ["ibmPlex", "inter", "roboto", "openSans", "montserrat", "sourceSans3", "workSans"],
+  Classy: ["lato", "merriweather", "cabin", "ubuntu", "ptSans"],
+  Elegant: ["poppins", "raleway", "playfair", "cormorant"],
+  Experimental: ["nunito", "josefinSans"],
+  Legacy: ["calligraphy"],
+};
+
+export const getFontName = (value) =>
+  Object.values(fonts).find((item) => item.value === value)?.name || value || "IBM Plex Sans";
